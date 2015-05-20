e849674f2cf0b9aa309f44225821c887a3161df1
sourbh-daffodilsw <sourbh.gupta@daffodilsw.com>
2015-05-20 16:48:37 +0530

    asdf

---
+++ www/CoolPlugin.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sourabh on 20/5/15.
+ */
+var exec = require('cordova/exec');
+function CoolPlugin() { console.log("CoolPlugin.js: is created");
+}
+CoolPlugin.prototype.showToast = function(aString){ console.log("CoolPlugin.js: showToast"); exec(function(result){ /*alert("OK" + reply);*/ }, function(result){ /*alert("Error" + reply);*/ },"CoolPlugin",aString,[]);
+} var coolPlugin = new CoolPlugin(); module.exports = coolPlugin;
