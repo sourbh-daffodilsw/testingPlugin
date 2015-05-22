f78b48769dd5cdd722cc541af5dcfed68cefd8ef
sourbh-daffodilsw <sourbh.gupta@daffodilsw.com>
2015-05-22 13:26:43 +0530

    android changes

--- src/android/sessionVariable.java
+++ src/android/sessionVariable.java
@@ -5,12 +5,15 @@ import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
+
 import android.util.Log;
 import android.provider.Settings;
 import android.widget.Toast;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -18,17 +21,16 @@ import android.content.SharedPreferences;
 public class sessionVariable extends CordovaPlugin {
     public static final String TAG = "session Variable";
     private SharedPreferences pref;
-    pref.Editor editor;
+    SharedPreferences.Editor editor;
 
     public sessionVariable() {
         pref = cordova.getActivity().getApplicationContext().getSharedPreferences("com.example.sourabh.trackinguser", 0); // 0 - for private mode
         editor = pref.edit();
     }
 
-}
-
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        webView.loadUrl("javascript:console.log('hello');");
         Log.v(TAG, "Init storage");
     }
 
@@ -41,43 +43,44 @@ public class sessionVariable extends CordovaPlugin {
                 Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
                 toast.show();
             }
+        });
 
-
-            if(action.equals("setKey"))
-                    editor.putString(args.getString(0),args.getString(1));
+        if (action.equals("setKey")) {
+            editor.putString(args.getString(0), args.getString(1));
             editor.commit();
-            cordova.getActivity().
-
-            runOnUiThread(new Runnable() {
-                public void run () {
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
                     Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), "VARIABLE SAVED", duration);
                     toast.show();
                 }
-            }
+            });
+            return true;
+        }
 
-            if(action.equals("getKey"))
-            String value = pref.getString(args.getString(0)),"not found");
+        if (action.equals("getKey")) {
+            final String value = pref.getString(args.getString(0), "not found");
             cordova.getActivity().runOnUiThread(new Runnable() {
-                public void run () {
+                public void run() {
                     Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), value, duration);
                     toast.show();
                 }
+            });
 //                return value;
-            }
+            return true;
+        }
 
-            if(action.equals("removeKey"))
-                    editor.remove(args.getString(0)));
+        if (action.equals("removeKey")){
+            editor.remove(args.getString(0));
             editor.commit();
-            cordova.getActivity().
-
-            runOnUiThread(new Runnable() {
-                public void run () {
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
                     Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), "VARIABLE REMOVED", duration);
                     toast.show();
                 }
-            }
-        });
-        return true;
+            });
+            return true;
+        }
+        return false;
     }
 }
 
--- www/sessionVariable.js
+++ www/sessionVariable.js
@@ -9,15 +9,15 @@ var functionExport = {};
 function sessionVariable() { console.log("sessionVariable.js: is created");
 }
 
-functionExport.setKey = function(key, value, successCallback, errorCallback) {
+functionExport.setKey = function(successCallback, errorCallback, key, value) {
     exec(successCallback, errorCallback, "sessionVariable", "setKey", [key, value]);
 }
 
-functionExport.getKey = function(key, successCallback, errorCallback) {
+functionExport.getKey = function(successCallback, errorCallback, key) {
     exec(successCallback, errorCallback, "sessionVariable", "getKey", [key]);
 }
 
-functionExport.removeKey = function(key, successCallback, errorCallback) {
+functionExport.removeKey = function(successCallback, errorCallback, key) {
     exec(successCallback, errorCallback, "sessionVariable", "removeKey", [key]);
 }
 
